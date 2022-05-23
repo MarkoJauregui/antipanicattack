bb5f029092d9f225a37f9c4c2916babe3f97d4f9
Misty-Dev <markojau@gmail.com>
2022-05-23 15:03:40 -0300

    remove comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 // eslint-disable-next-line no-undef
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 	const accounts = await hre.ethers.getSigners();
@@ -14,9 +12,6 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 		console.log(account.address);
 	}
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
