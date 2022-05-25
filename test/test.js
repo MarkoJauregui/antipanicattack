6104faab81b6aa3946f409a9c3640b70a1ebe5e3
Misty-Dev <markojau@gmail.com>
2022-05-25 18:16:30 -0300

    setPublicMint require test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('AntiPanicAttackNFT Contract', function () {
-	let AntiPanicAttackNFT, owner, addr1, addr2, antiPanicAttackNFT;
+	let AntiPanicAttackNFT, owner, addr1, addr2, addr3, antiPanicAttackNFT;
 
 	beforeEach(async () => {
-		[owner, addr1, addr2] = await ethers.getSigners();
+		[owner, addr1, addr2, addr3] = await ethers.getSigners();
 
 		AntiPanicAttackNFT = await ethers.getContractFactory(
 			'AntiPanicAttackNFT',
@@ -25,6 +25,17 @@ describe('AntiPanicAttackNFT Contract', function () {
 
 		it('should be the right symbol', async () => {
 			expect(await antiPanicAttackNFT.symbol()).to.equal('APA');
+		});
+	});
+
+	describe('mint Function', () => {
+		it('should NOT allow the user to mint while setPublicMint is false', async () => {
+			await antiPanicAttackNFT.connect(owner).setIsPublicMintEnabled(false);
+			await expect(
+				antiPanicAttackNFT.connect(addr1).mint(1, {
+					value: ethers.utils.parseEther('0.1'),
+				})
+			).to.be.revertedWith('Minting not enabled');
 		});
 	});
 });
